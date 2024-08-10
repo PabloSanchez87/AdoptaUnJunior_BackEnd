13bea333acc7b744440938adb2575009302c0ee1
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-10 20:32:01 +0200

    Añadimos las app a settings y urls del proyecto

--- backend_adopta_un_junior/backend_adopta_un_junior/settings.py
+++ backend_adopta_un_junior/backend_adopta_un_junior/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps
+    'motivation_app',
+    'learning_path_app',
+    'goals_app',
 ]
 
 MIDDLEWARE = [
