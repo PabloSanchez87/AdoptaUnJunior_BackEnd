8917bd229337739cf50e75540f7b897719bf0d8b
Guti <gutifer666@gmail.com>
2024-08-13 11:01:47 +0200

    vista modificada

--- backend_adopta_un_junior/goals_app/views.py
+++ backend_adopta_un_junior/goals_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Goal, Task
 
 def goals_list(request):
@@ -8,3 +9,12 @@ def goals_list(request):
 def tasks_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks_list.html', {'tasks': tasks})
+
+def change_task_status(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        new_status = request.POST.get('status')
+        task.status = new_status
+        task.save()
+        return redirect('tasks_list')
+    return HttpResponse(status=405)
