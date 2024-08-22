6a402eb4ddb5db25fa88509dab1f29480a34365b
Felix Serrano <exilefe@hotmail.com>
2024-08-22 15:35:45 +0200

    added code for issue

--- backend_adopta_un_junior/goals_app/urls.py
+++ backend_adopta_un_junior/goals_app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('goals/', views.goals_list, name='goals_list'),
     path('tasks/', views.tasks_list, name='tasks_list'),
+    path('change-task-status/<int:task_id>/', 
+            views.change_task_status, 
+            name='change_task_status'),
 ]
--- backend_adopta_un_junior/goals_app/views.py
+++ backend_adopta_un_junior/goals_app/views.py
@@ -8,3 +8,13 @@ def goals_list(request):
 def tasks_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks_list.html', {'tasks': tasks})
+
+
+def change_task_status(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        new_status = request.POST.get('status')
+        task.status = new_status
+        task.save()
+        return redirect('tasks_list')  # Redirige a la lista de tareas después de actualizar el estado
+    return HttpResponse(status=405)  # Método no permitido
