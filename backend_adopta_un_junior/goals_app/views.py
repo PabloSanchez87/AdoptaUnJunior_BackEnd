a777190565d4f2fb667544daf20c46537d106828
PabloSanchez <sancheztorrespablo@gmail.com>
2024-09-02 14:15:44 +0200

    importaciones views.py

--- backend_adopta_un_junior/goals_app/views.py
+++ backend_adopta_un_junior/goals_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect, get_object_or_404
 from .models import Goal, Task
+from django.http import HttpResponse
 
 def goals_list(request):
     goals = Goal.objects.all()
